aa0cc5326438594913ec559d853f9bf98e5cf386
xoyegon835@cristout.com <xoyegon835@cristout.com>
2025-06-26 14:39:23 +0000

    added the changes

---
+++ models/test.sql
@@ -0,0 +1,11 @@
+Select 
+CUSTOMER_ID as Cust_date,
+	FIRST_NAME as Cust_FName,
+	LAST_NAME as Cust_LName,
+	PHONE as Cust_Phone,
+	EMAIL as Cust_Emial,
+	STREET,
+	CITY,
+	STATE,
+	ZIP_CODE,
+from BIKESTORES.SALES.CUSTOMERS
